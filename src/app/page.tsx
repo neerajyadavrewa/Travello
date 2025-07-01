92c13a1690a3290ba90050c788efa269623f4270
neerajyadavrewa <neerajyadavrewa@gmail.com>
2025-07-01 18:28:21 +0530

    twelveth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ import { User } from "../../models/User";
 import PopularPackagesSection from "@/components/PopularPackagesSection/PopularPackagesSection";
 
 
- export const revalidate = 60 * 60; 
+ export const revalidate = 3600; 
 
 
 
